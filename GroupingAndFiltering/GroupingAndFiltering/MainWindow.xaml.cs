c47b2f2be8f536a8dee2da2332716d5c4bcd0db8
Rakesh Garia <Rakesh.Garia@aurora-digital.net>
2020-09-09 23:08:13 +0530

    Add logic for filtering and grouping

--- GroupingAndFiltering/GroupingAndFiltering/MainWindow.xaml.cs
+++ GroupingAndFiltering/GroupingAndFiltering/MainWindow.xaml.cs
@@ -1,28 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace GroupingAndFiltering
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            string sConn = @"Server=(local)\AURORA; Database=AdventureWorks2012;Trusted_Connection=true";
+            SqlConnection conn = new SqlConnection(sConn);
+            string sSQL = "SELECT * FROM [Production].[Product] ORDER BY ProductLine, Name";
+            SqlCommand comm;
+            SqlDataReader DR;
+            DataTable DT = new DataTable();
+
+            conn.Open();
+            comm = new SqlCommand(sSQL, conn);
+            DR = comm.ExecuteReader();
+            DT.Load(DR);
+            LayoutGrid.DataContext = DT;
+            DR.Close();
+            conn.Close();
+        }
+
+        private void GroupCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            ICollectionView view = CollectionViewSource.GetDefaultView(LayoutGrid.DataContext);
+            view.GroupDescriptions.Clear();
+            view.GroupDescriptions.Add(new PropertyGroupDescription("ProductLine"));
+        }
+
+        private void GroupCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            ICollectionView view = CollectionViewSource.GetDefaultView(LayoutGrid.DataContext);
+            view.GroupDescriptions.Clear();
+        }
+
+        private void FilterCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            ((DataTable)LayoutGrid.DataContext).DefaultView.RowFilter = "Color='Black'";
+        }
+
+        private void FilterCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            ((DataTable)LayoutGrid.DataContext).DefaultView.RowFilter = "";
         }
     }
 }
