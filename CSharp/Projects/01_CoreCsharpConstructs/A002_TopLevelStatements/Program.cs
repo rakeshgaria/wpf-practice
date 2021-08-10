546073c4e94c256a85d899236b0f996f9f6ee5d1
Rakesh Garia <rakesh.garia@gmail.com>
2021-08-11 00:22:02 +0530

    Top Level Statement is a new feature in C# 9.0

---
+++ CSharp/Projects/01_CoreCsharpConstructs/A002_TopLevelStatements/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+
+// Display a simple message to the user.
+Console.WriteLine("***** My First C# App *****");
+Console.WriteLine("Hello World!");
+Console.WriteLine();
+// Wait for Enter key to be pressed before shutting down.
+Console.ReadLine();
+
+foreach (var arg in args)
+{
+    Console.WriteLine(arg);
+}
+
+for (int i = 0; i < 20; i++)
+{
+    Console.Write("| -");
+    await Task.Delay(50);
+    Console.Write("\b\b\b");
+    Console.Write("/ \\");
+    await Task.Delay(50);
+    Console.Write("\b\b\b");
+    Console.Write("- |");
+    await Task.Delay(50);
+    Console.Write("\b\b\b");
+    Console.Write("\\ /");
+    await Task.Delay(50);
+    Console.Write("\b\b\b");
+}
+Console.WriteLine();
