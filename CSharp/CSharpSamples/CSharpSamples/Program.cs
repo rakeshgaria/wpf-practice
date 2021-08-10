c027bf33b00590d44b8cd2ee7829398e06b77dca
Rakesh Garia <rakesh.garia@gmail.com>
2021-08-10 22:08:03 +0530

    Using variations of the Main() method

---
+++ CSharp/CSharpSamples/CSharpSamples/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CSharpSamples
+{
+    // Using variations of the Main() method
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Program2
+    {
+        static int Main(string[] args)
+        {
+            return 0;
+        }
+    }
+
+    class Program3
+    {
+        static void Main()
+        {
+
+        }
+    }
+
+    class Program4
+    {
+        static int Main()
+        {
+            return 0;
+        }
+    }
+
+    class Program5
+    {
+        static Task Main()
+        {
+            return new Task(() => { });
+        }
+    }
+
+    class Program6
+    {
+        static Task<int> Main()
+        {
+            return new Task<int>(Method);
+        }
+
+        static int Method()
+        {
+            return 0;
+        }
+    }
+
+    class Program7
+    {
+        static Task Main(string[] args)
+        {
+            return new Task(() => { });
+        }
+    }
+
+    class Program8
+    {
+        static Task<int> Main(string[] args)
+        {
+            return new Task<int>(Method);
+        }
+
+        static int Method()
+        {
+            return 0;
+        }
+    }
+}
